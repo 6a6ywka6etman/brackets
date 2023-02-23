b8411e1ef8cd4a01abc02d218204b2227e914862
Sapunov <ilsapunov@gmail.com>
2023-02-23 07:11:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+      return false;
+  }
+  let brackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      brackets[i] = bracketsConfig[i][0] + bracketsConfig[i][1];  
+  }
+  console.log(brackets);
+  let flag = 0;
+
+  while(str.length !==0){
+      flag = 0;
+      for (let i = 0; i < brackets.length; i++) {
+          if(str.indexOf(brackets[i]) !== -1){
+              str = str.replace(brackets[i], '');      
+          }else{
+              flag+=1;
+          }
+      }
+      if (flag === brackets.length) {
+          return false;
+      }
+  }
+  return true;
+
 }
